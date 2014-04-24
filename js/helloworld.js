3772e8cfb66c5826d6eb7274b96f58e97f6c27b7
nakanotaiji <nakanotaiji@nakanofutoshini-no-MacBook-Air.local>
2014-04-24 12:34:40 +0900

    論プロ課題

--- js/helloworld.js
+++ js/helloworld.js
@@ -5,3 +5,13 @@ var helloWorld = function(){
 
 var showHelloWorldButton = document.getElementById("showHelloWorld");
 showHelloWorldButton.addEventListener("click", helloWorld);
+
+
+var helloChiko = function(){
+	var message = document.getElementById("helloChiko");
+	message.setAttribute("class", "");
+};
+
+var showHelloChikoButton = document.getElementById("showHelloChiko");
+showHelloWorldButton.addEventListener("click", helloChiko);
+
